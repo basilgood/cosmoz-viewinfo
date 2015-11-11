15c9f51241dcb806ecb524aa5845d723cb041c1d
Patrik Kullman <patrik.kullman@neovici.se>
2015-11-11 11:06:35 +0100

    Initial commit

---
+++ cosmoz-viewinfo.js
@@ -0,0 +1,143 @@
+/*global Cosmoz, Polymer, ResizeSensor, window */
+
+if (typeof Cosmoz === 'undefined') {
+	var Cosmoz = {};
+}
+
+(function () {
+
+	"use strict";
+
+	var
+		viewInfoInstances = [],
+		sharedViewInfo = {
+			desktop: false,
+			effects: 10,
+			height: 0,
+			mobile: false,
+			tablet: false,
+			width: 0
+		};
+
+	/**
+	 * Behavior to inherit the viewInfo property with device and viewport info
+	 * @type {Object}
+	 @polymerBehavior
+	 */
+	Cosmoz.ViewInfoBehavior = {
+		properties: {
+			viewInfo: {
+				type: Object,
+				notify: true
+			}
+		},
+
+		attached: function () {
+			viewInfoInstances.push(this);
+			// Needed to make template views trigger on load and not only on resize
+			this.viewInfo = sharedViewInfo;
+		},
+
+		detached: function () {
+			var i = viewInfoInstances.indexOf(this);
+			if (i >= 0) {
+				viewInfoInstances.splice(i, 1);
+			}
+		}
+	};
+
+	Polymer({
+		is: 'cosmoz-viewinfo',
+		properties: {
+			effects: {
+				type: Number,
+				value: 10,
+				observer: '_effectsChanged'
+			},
+			_sensor: {
+				type: Object
+			},
+			throttleTimeout: {
+				type: Number,
+				value: 250
+			},
+			_throttling: {
+				type: Boolean,
+				value: false
+			}
+		},
+		attached: function () {
+			this._sensor = new ResizeSensor(this, this._onResize.bind(this));
+			this._onResize();
+		},
+		_effectsChanged: function () {
+			sharedViewInfo.effects = this.effects;
+			this._notifyInstances();
+		},
+		_notifyInstances: function () {
+			viewInfoInstances.forEach(function (instance) {
+				if (!instance) {
+					return;
+				}
+				instance.set('viewInfo', {});
+				instance.set('viewInfo', sharedViewInfo);
+			}.bind(this));
+		},
+		_onResize: function () {
+			var
+				update = this._updateViewSize(),
+				throttleFunction;
+
+			if (update === undefined) {
+				return;
+			}
+
+			throttleFunction = function () {
+				viewInfoInstances.forEach(function (element) {
+					// Only fire on visible elements, offsetParent should be null for hidden
+					// https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/offsetParent
+					if (element.offsetParent !== null) {
+						element.fire('viewinfo-resize', {
+							bigger: update
+						});
+					}
+				}.bind(this));
+				this._throttling = false;
+			}.bind(this);
+
+			if (!this._throttling) {
+				window.setTimeout(throttleFunction, this.throttleTimeout);
+				this._throttling = true;
+			}
+		},
+		_updateViewSize: function () {
+			var
+				oldWidth = sharedViewInfo.width,
+				width = this.clientWidth || this.offsetWidth || this.scrollWidth;
+
+			sharedViewInfo.height = this.clientHeight || this.offsetHeight || this.scrollHeight;
+
+			if (oldWidth === width) {
+				return;
+			}
+
+			if (width <= 640) {
+				sharedViewInfo.mobile = true;
+				sharedViewInfo.tablet = false;
+			} else if (width <= 900) {
+				sharedViewInfo.mobile = false;
+				sharedViewInfo.tablet = true;
+			} else {
+				sharedViewInfo.mobile = false;
+				sharedViewInfo.tablet = false;
+			}
+
+			sharedViewInfo.desktop = !sharedViewInfo.mobile && !sharedViewInfo.tablet;
+			sharedViewInfo.width = width;
+
+			this._notifyInstances();
+
+			return oldWidth < width;
+		}
+	});
+}());
